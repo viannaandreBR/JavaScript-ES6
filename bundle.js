8025fa6a7eff0eb8ba3f45be02c37957b5ecac67
viannaandreBR <andre@bioinformaticalabs.com>
2019-08-22 15:37:36 -0300

    Updade: Manipulação de Array - Somando Item+ Index

--- bundle.js
+++ bundle.js
@@ -1,8 +1,8 @@
 "use strict";
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var newArr = arr.map(function (item) {
-  return item * 4;
+var newArr = arr.map(function (item, index) {
+  return item + index;
 });
 alert(newArr);
 console.log(newArr);
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const newArr = arr.map(function(item){
-    return item * 4;
+const newArr = arr.map(function(item, index){
+    return item + index;
 });
 
 alert(newArr);
