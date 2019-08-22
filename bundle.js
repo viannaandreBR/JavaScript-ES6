5cb60d802b8593219b697315f21f44085e1e3669
viannaandreBR <andre@bioinformaticalabs.com>
2019-08-22 15:33:54 -0300

    Update: Manipulação de Array 2

--- bundle.js
+++ bundle.js
@@ -2,6 +2,7 @@
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var newArr = arr.map(function (item) {
-  return item * 2;
+  return item * 4;
 });
+alert(newArr);
 console.log(newArr);
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const newArr = arr.map(function(item){
-    return item * 2;
+    return item * 4;
 });
 
+alert(newArr);
 console.log(newArr);
