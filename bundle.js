712a463fd41fc664ddf08e01597351c0eba27987
viannaandreBR <andre@bioinformaticalabs.com>
2019-08-22 01:56:18 -0300

    Add index.html button & main.js TodoList Class

--- bundle.js
+++ bundle.js
@@ -6,22 +6,28 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-alert('Teste');
-
-var Teste =
+var TodoList =
 /*#__PURE__*/
 function () {
-  function Teste() {
-    _classCallCheck(this, Teste);
+  function TodoList() {
+    _classCallCheck(this, TodoList);
+
+    this.todos = [];
   }
 
-  _createClass(Teste, [{
-    key: "method",
-    value: function method() {}
-  }, {
-    key: "outro",
-    value: function outro() {}
+  _createClass(TodoList, [{
+    key: "addTodo",
+    value: function addTodo() {
+      this.todos.push('Novo todo');
+      console.log(this.todos);
+    }
   }]);
 
-  return Teste;
+  return TodoList;
 }();
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+  MinhaLista.addTodo();
+};
--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-alert('Teste');
-
-class Teste {
-    method () {
-
+class TodoList {
+    constructor() {
+        this.todos =[];
     }
 
-    outro () {
-        
+    addTodo() {
+        this.todos.push('Novo todo');
+        console.log(this.todos);
     }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.addTodo();
 }
