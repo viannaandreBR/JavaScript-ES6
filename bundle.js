f8e978f10bbea366f6b7fd64fa624332cfd4bc8b
viannaandreBR <andre@bioinformaticalabs.com>
2019-08-22 15:46:47 -0300

    Updade: Manipulação Array com Reduce

--- bundle.js
+++ bundle.js
@@ -1,8 +1,13 @@
 "use strict";
 
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //const arr = [1,3,4,5,8,9];
+
 var newArr = arr.map(function (item, index) {
   return item + index;
 });
 alert(newArr);
 console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+//const arr = [1,3,4,5,8,9];
+
 const newArr = arr.map(function(item, index){
     return item + index;
 });
 
 alert(newArr);
 console.log(newArr);
+
+const sum = arr.reduce(function(total,next){
+    return total + next;
+})
+
+console.log(sum);
