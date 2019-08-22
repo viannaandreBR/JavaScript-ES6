34b2a793def785609bd9a7c8d6eaf69a11122ae4
viannaandreBR <andre@bioinformaticalabs.com>
2019-08-22 16:05:02 -0300

    Updade: Find, Filter, Reduce

--- bundle.js
+++ bundle.js
@@ -1,13 +1,30 @@
 "use strict";
 
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //const arr = [1,3,4,5,8,9];
+var arr = [1, 4, 3, 4, 5, 6, 7, 8, 9, 10]; //const arr = [1,3,4,5,8,9];
 
 var newArr = arr.map(function (item, index) {
   return item + index;
-});
+}); // soma elementos Array com reduce
+
 alert(newArr);
 console.log(newArr);
 var sum = arr.reduce(function (total, next) {
   return total + next;
 });
-console.log(sum);
+console.log(sum); // Identifica Pares com filter
+
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log('Elementos pares', filter); // Identifica Impares com filter
+
+var nfilter = arr.filter(function (item) {
+  return item % 2 === 1;
+});
+console.log('Elementos impares', nfilter); // Tamanho do Array
+// Funcionalidade Find
+
+var find = arr.find(function (index) {
+  return index === 4;
+});
+console.log('Localizando o 4', find);
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr = [1, 4, 3, 4, 5, 6, 7, 8, 9, 10];
 
 //const arr = [1,3,4,5,8,9];
 
 const newArr = arr.map(function(item, index){
     return item + index;
 });
+
+// soma elementos Array com reduce
 
 alert(newArr);
 console.log(newArr);
@@ -14,3 +16,28 @@ const sum = arr.reduce(function(total,next){
 })
 
 console.log(sum);
+
+// Identifica Pares com filter
+const filter = arr.filter(function(item)
+{
+    return item % 2 === 0;
+});
+console.log('Elementos pares',filter);
+
+// Identifica Impares com filter
+const nfilter = arr.filter(function(item)
+{
+    return item %2 === 1;
+});
+
+console.log('Elementos impares',nfilter);
+
+// Tamanho do Array
+
+// Funcionalidade Find
+
+const find = arr.find(function(index)
+{
+    return index === 4;
+})
+console.log('Localizando o 4',find);
