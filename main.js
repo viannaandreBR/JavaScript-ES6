f1585a7d1e7547facc0855e1800c2287859552ac
viannaandreBR <andre@bioinformaticalabs.com>
2019-08-23 02:57:48 -0300

    Checkpoint: Build WebPack

--- main.js
+++
@@ -1,5 +0,0 @@
-import * as funcoes from './funcoes';
-
-console.log('Soma          =>',funcoes.soma(4,3));
-console.log('Subtração     =>',funcoes.sub(4,3));
-console.log('Multiplicação =>',funcoes.mult(4,3));
