493e7ba2503f1d4ffc2679803e200aa140f65abe
viannaandreBR <andre@bioinformaticalabs.com>
2019-08-23 21:35:00 -0300

    API GitHub - Using Try/Catch(err)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 document.write('API - Using Axios and Async / Await<br><br><br>');
+console.log('Método Try/Catch(errr)');
 //
 //
 // Crição de API utilizando o Axios e Async/Await
@@ -6,14 +7,19 @@ document.write('API - Using Axios and Async / Await<br><br><br>');
 //
 import axios from 'axios';
 
+
 class Api {
     static async getUserInfo(username){
+        try {
         const response = await axios.get(`https://api.github.com/users/${username}`);
-    
         console.log(response);
+        } catch (err) {
+        console.warn('Erro na API');
+        }
 
-//document.write(response);
     }
+
+
 }
 
-Api.getUserInfo('viannaandreBR');
+Api.getUserInfo('viannaandreBR233');
