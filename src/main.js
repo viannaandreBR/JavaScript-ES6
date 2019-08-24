afb419f1c130e57e381d6bd142cb6b619f4a7af2
viannaandreBR <andre@bioinformaticalabs.com>
2019-08-24 02:55:31 -0300

    Update: App Render Main.js & index.html

--- src/main.js
+++ src/main.js
@@ -4,37 +4,63 @@
 
 //document.write('App consulta API GitHub');
 
-console.log('App consulta API GitHub');
-
 class App {
     constructor(){
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
-
+        this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
 
 
-    }
+    };
 
     registerHandlers() {
         this.formEl.onsubmit = event => this.addRepository(event);
 
-    }
+    };
 
-    addRepository() {
+    addRepository(event) {
         event.preventDefault();
 
         this.repositories.push({
             name: 'rocketseat.com.br',
-            despription: 'Tire a sua idéia do papel e dê vida à sua startup',
+            description:'Tire a sua idéia do papel e dê vida à sua startup',
             avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url: 'http://github.com/rocketseat.com.brrocketseat.com.br'
+            html_url: 'http://github.com/rocketseat.com.brrocketseat.com.br',
         });
 
-        console.log(this.repositories);
-        
+        this.render();
+    };
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target','_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
+
+
     }
 }
 
